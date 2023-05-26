114d54669da59d110e382296d736e81c36432e5c
n0ur38 <123877125+n0ur38@users.noreply.github.com>
2023-05-26 05:35:47 +0300

    Update locate.c

--- locate.c
+++ locate.c
@@ -1,30 +1,23 @@
-/*
- * File: locate.c
- * 
- *          
- */
-
 #include "shell.h"
 
 char *fill_path_dir(char *path);
 list_t *get_path_dir(char *path);
 
 /**
- * get_location - Locates a command in the PATH.
- * @command: The command to locate.
- *
- * Return: If an error occurs or the command cannot be located - NULL.
- *         Otherwise - the full pathname of the command.
- */
+* get_location - Locates a command in the PATH.
+* @command: The command to locate.
+*
+* Return: If an error occurs or the command cannot be located - NULL.
+*         Otherwise - the full pathname of the command.
+*/
 char *get_location(char *command)
 {
-	char **path, *temp;
-	list_t *dirs, *head;
-	struct stat st;
-
-	path = _getenv("PATH");
-	if (!path || !(*path))
-		return (NULL);
+char **path, *temp;
+list_t *dirs, *head;
+struct stat st;
+path = _getenv("PATH");
+if (!path || !(*path))
+return (NULL);
 
 	dirs = get_path_dir(*path + 5);
 	head = dirs;
@@ -110,12 +103,12 @@ char *fill_path_dir(char *path)
 }
 
 /**
- * get_path_dir - Tokenizes a colon-separated list of
- *                directories into a list_s linked list.
- * @path: The colon-separated list of directories.
- *
- * Return: A pointer to the initialized linked list.
- */
+* get_path_dir - Tokenizes a colon-separated list of
+*                directories into a list_s linked list.
+* @path: The colon-separated list of directories.
+*
+* Return: A pointer to the initialized linked list.
+*/
 list_t *get_path_dir(char *path)
 {
 	int index;
