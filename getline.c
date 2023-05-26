06532a84c3342abaae524bc3badded13c81be303
n0ur38 <123877125+n0ur38@users.noreply.github.com>
2023-05-27 02:46:09 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,7 +1,7 @@
 #include "shell.h"
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *_realloc(void *pointr, unsigned int old_size, unsigned int new_size);
+void assign_lineptr(char **linepointr, size_t *n, char *buffer, size_t b);
+ssize_t _getline(char **linepointr, size_t *n, FILE *stream);
 
 /**
 * _realloc - Reallocates a memory block using malloc and free.
@@ -9,40 +9,40 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 * @old_size: The size in bytes of the allocated space for ptr.
 * @new_size: The size in bytes for the new memory block.
 *
-* Return: If new_size == old_size - ptr.
+* return: If new_size == old_size - ptr.
 *         If new_size == 0 and ptr is not NULL - NULL.
 *         Otherwise - a pointer to the reallocated memory block.
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 void *mem;
-char *ptr_copy, *filler;
-unsigned int index;
+char *pointr_copy, *filler;
+unsigned int indexx;
 if (new_size == old_size)
-return (ptr);
-if (ptr == NULL)
+return (pointr);
+if (pointr == NULL)
 {
 mem = malloc(new_size);
 if (mem == NULL)
 return (NULL);
 return (mem);
 }
-if (new_size == 0 && ptr != NULL)
+if (new_size == 0 && pointr != NULL)
 {
-free(ptr);
+free(pointr);
 return (NULL);
 }
-ptr_copy = ptr;
-mem = malloc(sizeof(*ptr_copy) * new_size);
+pointr_copy = pointr;
+mem = malloc(sizeof(*pointr_copy) * new_size);
 if (mem == NULL)
 {
-free(ptr);
+free(pointr);
 return (NULL);
 }
 filler = mem;
-for (index = 0; index < old_size && index < new_size; index++)
-filler[index] = *ptr_copy++;
-free(ptr);
+for (indexx = 0; indexx < old_size && indexx < new_size; indexx++)
+filler[indexx] = *pointr_copy++;
+free(pointr);
 return (mem);
 }
 /**
@@ -52,15 +52,15 @@ return (mem);
 * @buffer: The string to assign to lineptr.
 * @b: The size of buffer.
 */
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
+void assign_linepointr(char **linepointr, size_t *n, char *buffer, size_t b)
 {
-if (*lineptr == NULL)
+if (*linepointr == NULL)
 {
 if (b > 120)
 *n = b;
 else
 *n = 120;
-*lineptr = buffer;
+*linepointr  = buffer;
 }
 else if (*n < b)
 {
@@ -68,11 +68,11 @@ if (b > 120)
 *n = b;
 else
 *n = 120;
-*lineptr = buffer;
+*linepointr = buffer;
 }
 else
 {
-_strcpy(*lineptr, buffer);
+_strcpy(*linepointr, buffer);
 free(buffer);
 }
 }
@@ -84,7 +84,7 @@ free(buffer);
 *
 * Return: The number of bytes read.
 */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+ssize_t _getline(char **linepointr, size_t *n, FILE *stream)
 {
 static ssize_t input;
 ssize_t ret;
@@ -117,7 +117,7 @@ buffer[input] = c;
 input++;
 }
 buffer[input] = '\0';
-assign_lineptr(lineptr, n, buffer, input);
+assign_linepointr(linepointr, n, buffer, input);
 ret = input;
 if (r != 0)
 input = 0;
