1a962582435f49c85cc5308a63aced118b06219a
n0ur38 <123877125+n0ur38@users.noreply.github.com>
2023-05-26 05:38:01 +0300

    Update helpers_2.c

--- helpers_2.c
+++ helpers_2.c
@@ -1,9 +1,3 @@
-/*
- * File: helpers_2.c
- * 
- *         
- */
-
 #include "shell.h"
 
 void handle_line(char **line, ssize_t read);
@@ -11,26 +5,25 @@ ssize_t get_new_len(char *line);
 void logical_ops(char *line, ssize_t *new_len);
 
 /**
- * handle_line - Partitions a line read from standard input as needed.
- * @line: A pointer to a line read from standard input.
- * @read: The length of line.
- *
- * Description: Spaces are inserted to separate ";", "||", and "&&".
- *              Replaces "#" with '\0'.
- */
+* handle_line - Partitions a line read from standard input as needed.
+* @line: A pointer to a line read from standard input.
+* @read: The length of line.
+*
+* Description: Spaces are inserted to separate ";", "||", and "&&".
+*              Replaces "#" with '\0'.
+*/
 void handle_line(char **line, ssize_t read)
 {
-	char *old_line, *new_line;
-	char previous, current, next;
-	size_t i, j;
-	ssize_t new_len;
-
-	new_len = get_new_len(*line);
-	if (new_len == read - 1)
-		return;
-	new_line = malloc(new_len + 1);
-	if (!new_line)
-		return;
+char *old_line, *new_line;
+char previous, current, next;
+size_t i, j;
+ssize_t new_len;
+new_len = get_new_len(*line);
+if (new_len == read - 1)
+return;
+new_line = malloc(new_len + 1);
+if (!new_line)
+return;
 	j = 0;
 	old_line = *line;
 	for (i = 0; old_line[i]; i++)
@@ -102,14 +95,14 @@ void handle_line(char **line, ssize_t read)
 }
 
 /**
- * get_new_len - Gets the new length of a line partitioned
- *               by ";", "||", "&&&", or "#".
- * @line: The line to check.
- *
- * Return: The new length of the line.
- *
- * Description: Cuts short lines containing '#' comments with '\0'.
- */
+* get_new_len - Gets the new length of a line partitioned
+*               by ";", "||", "&&&", or "#".
+* @line: The line to check.
+*
+* Return: The new length of the line.
+*
+* Description: Cuts short lines containing '#' comments with '\0'.
+*/
 
 ssize_t get_new_len(char *line)
 {
@@ -163,10 +156,10 @@ ssize_t get_new_len(char *line)
 	return (new_len);
 }
 /**
- * logical_ops - Checks a line for logical operators "||" or "&&".
- * @line: A pointer to the character to check in the line.
- * @new_len: Pointer to new_len in get_new_len function.
- */
+* logical_ops - Checks a line for logical operators "||" or "&&".
+* @line: A pointer to the character to check in the line.
+* @new_len: Pointer to new_len in get_new_len function.
+*/
 void logical_ops(char *line, ssize_t *new_len)
 {
 	char previous, current, next;
