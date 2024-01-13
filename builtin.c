9a46870a89a0217cbfe9e0d2b38bd570432c6629
khlifaelbad <khlifaelbad@gmail.com>
2024-01-12 16:17:03 -0800

    Simple_shell_project

--- builtin.c
+++ builtin.c
@@ -1,193 +1,117 @@
-nclude "shell.h"
-int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
+#include "main.h"
 
 /**
- *  * get_builtin - Matches a command with a corresponding
- *   *               shellby builtin function.
- *    * @command: The command to match.
- *     *
- *      * Return: A function pointer to the corresponding builtin.
- *       */
-int (*get_builtin(char *command))(char **args, char **front)
+ * exec_builtin - check if built in and then exec
+ * @d: data struct input
+ * Return: 1 if built in, 0 if not
+ */
+int exec_builtin(data *d)
 {
-		builtin_t funcs[] = {
-					{ "exit", shellby_exit },
-							{ "env", shellby_env },
-									{ "setenv", shellby_setenv },
-											{ "unsetenv", shellby_unsetenv },
-													{ "cd", shellby_cd },
-															{ "alias", shellby_alias },
-																	{ "help", shellby_help },
-																			{ NULL, NULL }
-						};
-			int i;
+	builtin builtin[] = {
+		{"exit", builtin_exit},
+		{"env", builtin_env},
+		{"setenv", builtin_setenv},
+		{"unsetenv", builtin_unsetenv},
+		{"cd", builtin_cd},
+		{NULL, NULL},
+	};
+	int i = 0;
 
-				for (i = 0; funcs[i].name; i++)
-						{
-									if (_strcmp(funcs[i].name, command) == 0)
-													break;
-										}
-					return (funcs[i].f);
+	for (i = 0; builtin[i].cmd; i++)
+	{
+		if (_strcmp(d->av[0], builtin[i].cmd) == 0)
+		{
+			builtin[i].f(d);
+			return (1);
+		}
+	}
+	return (0);
 }
 
 /**
- *  * shellby_exit - Causes normal process termination
- *   *                for the shellby shell.
- *    * @args: An array of arguments containing the exit value.
- *     * @front: A double pointer to the beginning of args.
- *      *
- *       * Return: If there are no arguments - -3.
- *        *         If the given exit value is invalid - 2.
- *         *         O/w - exits with the given status value.
- *          *
- *           * Description: Upon returning -3, the program exits back in the main function.
- *            */
-int shellby_exit(char **args, char **front)
+ * builtin_exit - exits the shell
+ * @d: data struct input
+ * Return: void
+ */
+void builtin_exit(data *d)
 {
-		int i, len_of_int = 10;
-			unsigned int num = 0, max = 1 << (sizeof(int) * 8 - 1);
+	const char *errorString = "./hsh: 1: exit: Illegal number: ";
 
-				if (args[0])
-						{
-									if (args[0][0] == '+')
-												{
-																i = 1;
-																			len_of_int++;
-																					}
-											for (; args[0][i]; i++)
-														{
-																		if (i <= len_of_int && args[0][i] >= '0' && args[0][i] <= '9')
-																							num = (num * 10) + (args[0][i] - '0');
-																					else
-																										return (create_error(--args, 2));
-																							}
-												}
-					else
-							{
-										return (-3);
-											}
-						if (num > max - 1)
-									return (create_error(--args, 2));
-							args -= 1;
-								free_args(args, front);
-									free_env();
-										free_alias_list(aliases);
-											exit(num);
+	if (d->av[1])
+	{
+		if (d->av[1][0] != '-' && _isnumber(d->av[1]))
+			d->last_exit_status = atoi(d->av[1]);
+		else
+		{
+			write(STDERR_FILENO, errorString, strlen(errorString));
+			write(STDERR_FILENO, d->av[1], strlen(d->av[1]));
+			write(STDERR_FILENO, "\n", 1);
+			d->last_exit_status = 2;
+		}
+	}
+	free_array(d->av);
+	free(d->cmd);
+	if (d->flag_setenv)
+		free_array(environ);
+	exit(d->last_exit_status);
 }
 
 /**
- *  * shellby_cd - Changes the current directory of the shellby process.
- *   * @args: An array of arguments.
- *    * @front: A double pointer to the beginning of args.
- *     *
- *      * Return: If the given string is not a directory - 2.
- *       *         If an error occurs - -1.
- *        *         Otherwise - 0.
- *         */
-int shellby_cd(char **args, char __attribute__((__unused__)) **front)
+ * builtin_env - prints the current environment
+ * @d: data struct input
+ * Return: void
+ */
+void builtin_env(data *d)
 {
-		char **dir_info, *new_line = "\n";
-			char *oldpwd = NULL, *pwd = NULL;
-				struct stat dir;
+	int i = 0;
 
-					oldpwd = getcwd(oldpwd, 0);
-						if (!oldpwd)
-									return (-1);
+	(void)d;
+	while (environ[i])
+	{
+		_printf(environ[i]);
+		_printf("\n");
+		i++;
+	}
+}
+/**
+ * builtin_setenv - Initialize a new environment variable,
+ * or modify an existing one
+ * @d: data struct input
+ * Return: void
+ */
 
-							if (args[0])
-									{
-												if (*(args[0]) == '-' || _strcmp(args[0], "--") == 0)
-															{
-																			if ((args[0][1] == '-' && args[0][2] == '\0') ||
-																										args[0][1] == '\0')
-																							{
-																												if (_getenv("OLDPWD") != NULL)
-																																		(chdir(*_getenv("OLDPWD") + 7));
-																															}
-																						else
-																										{
-																															free(oldpwd);
-																																			return (create_error(args, 2));
-																																						}
-																								}
-														else
-																	{
-																					if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
-																												&& ((dir.st_mode & S_IXUSR) != 0))
-																										chdir(args[0]);
-																								else
-																												{
-																																	free(oldpwd);
-																																					return (create_error(args, 2));
-																																								}
-																										}
-															}
-								else
-										{
-													if (_getenv("HOME") != NULL)
-																	chdir(*(_getenv("HOME")) + 5);
-														}
-
-									pwd = getcwd(pwd, 0);
-										if (!pwd)
-													return (-1);
-
-											dir_info = malloc(sizeof(char *) * 2);
-												if (!dir_info)
-															return (-1);
-
-													dir_info[0] = "OLDPWD";
-														dir_info[1] = oldpwd;
-															if (shellby_setenv(dir_info, dir_info) == -1)
-																		return (-1);
-
-																dir_info[0] = "PWD";
-																	dir_info[1] = pwd;
-																		if (shellby_setenv(dir_info, dir_info) == -1)
-																					return (-1);
-																			if (args[0] && args[0][0] == '-' && args[0][1] != '-')
-																					{
-																								write(STDOUT_FILENO, pwd, _strlen(pwd));
-																										write(STDOUT_FILENO, new_line, 1);
-																											}
-																				free(oldpwd);
-																					free(pwd);
-																						free(dir_info);
-																							return (0);
+void builtin_setenv(data *d)
+{
+	(void)d;
+	if (d->av[1] && d->av[2])
+	{
+		if (_setenv(d, d->av[1], d->av[2]) == -1)
+		{
+			perror("setenv");
+		}
+	}
 }
 
 /**
- *  * shellby_help - Displays information about shellby builtin commands.
- *   * @args: An array of arguments.
- *    * @front: A pointer to the beginning of args.
- *     *
- *      * Return: If an error occurs - -1.
- *       *         Otherwise - 0.
- *        */
-int shellby_help(char **args, char __attribute__((__unused__)) **front)
+ * builtin_unsetenv - Remove an environment variable
+ * @d: data struct input
+ * Return: void
+ */
+void builtin_unsetenv(data *d)
 {
-		if (!args[0])
-					help_all();
-			else if (_strcmp(args[0], "alias") == 0)
-						help_alias();
-				else if (_strcmp(args[0], "cd") == 0)
-							help_cd();
-					else if (_strcmp(args[0], "exit") == 0)
-								help_exit();
-						else if (_strcmp(args[0], "env") == 0)
-									help_env();
-							else if (_strcmp(args[0], "setenv") == 0)
-										help_setenv();
-								else if (_strcmp(args[0], "unsetenv") == 0)
-											help_unsetenv();
-									else if (_strcmp(args[0], "help") == 0)
-												help_help();
-										else
-													write(STDERR_FILENO, name, _strlen(name));
+	int i, j;
+	int len;
 
-											return (0);
+	(void)d;
+	if (!d->av[1] || !getenv(d->av[1]))
+	{
+		_perror(d->shell_name, "variable not found.");
+		return;
+	}
+	len = strlen(d->av[1]);
+	for (i = 0; environ[i]; i++)
+		if (strncmp(environ[i], d->av[1], len) == 0 && environ[i][len] == '=')
+			for (j = i; environ[j]; j++)
+				environ[j] = environ[j + 1];
 }
 
